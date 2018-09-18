d2fb5424922a7afbda164e3430d6cccd9f4d4785
chowfoon <chowfoon@163.com>
2018-09-18 21:33:35 +0800

    实现图片验证码

---
+++ info/modules/index/views.py
@@ -0,0 +1,14 @@
+from flask import current_app
+from flask import render_template
+
+from . import index_blu
+
+
+@index_blu.route('/')
+def index():
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from info import create_app, db, models
 
 
-app = Flask(__name__)
+# 创建 app,并传入配置模式: development/production
+app = create_app('development')
 
+# flask_script
+manager = Manager(app)
 
-@app.route('/index')
-def index():
-    return 'index'
+# 数据库迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    manager.run()
